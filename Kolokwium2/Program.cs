48dcbd928daf5601abce050909e87d37e5401f1d
ixon7 <55660649+ixon7@users.noreply.github.com>
2020-01-26 16:54:22 +0100

    Update Program.cs

--- Kolokwium2/Program.cs
+++ Kolokwium2/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Kolokwium2
 {
+    public class Komputer
+    {
+        public string Procesor, RAM, HDD, Grafika;
+        public Komputer(string Procesor, string RAM, string HDD, string Grafika)
+        {
+            return;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Kolokwium 2:");
+            for (int i = 0; i < 100; i++)
+            {
+
+            }
+            var rand = new Random();
+            //Komputer pc = new Komputer();
+
+            //Komputer pcexample = new Komputer();
+            IEnumerable<string> enumerable = new string[] {
+                
+            };
+            foreach (string s in enumerable)
+                Console.WriteLine(s);
         }
     }
 }
